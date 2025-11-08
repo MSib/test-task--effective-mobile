f22f3841ae199997ad1815d5591700a6b016f39b
MSib <msib.dev@mail.ru>
2025-11-08 19:52:42 +0500

    added error handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\JsonResponse;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +18,24 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function ($exceptions) {
+        $exceptions->render(function (ModelNotFoundException $e, $request) {
+            return new JsonResponse([
+                'status'  => 'error',
+                'message' => 'Resource not found',
+            ], 404);
+        });
+
+        $exceptions->render(function (ValidationException $e, $request) {
+            return new JsonResponse([
+                'message' => 'Validation Error',
+                'errors'  => $e->errors(),
+            ], 422);
+        });
+
+        $exceptions->render(function (Throwable $e, $request) {
+            return new JsonResponse([
+                'message' => 'Internal Server Error',
+            ], 500);
+        });
     })->create();
